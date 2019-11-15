992d78dd786504af629eb4755dcd65bdaa33a2e6
Tyler Ward <twardnw@gmail.com>
2019-11-15 14:08:19 -0800

    install from tgz

--- lagoon.rb
+++ lagoon.rb
@@ -1,8 +1,8 @@
 class Lagoon < Formula
   desc "Command line tool for interacting with a Lagoon API"
   homepage "https://github.com/amazeeio/lagoon-cli"
-  url "https://github.com/amazeeio/lagoon-cli/releases/download/0.2.0/lagoon-cli-0.2.0-darwin-amd64"
-  sha256 "3e4207d290daac9bcd3e00d954fbad38803332d1d6be50f05111452b71f6a30c"
+  url "https://github.com/amazeeio/lagoon-cli/releases/download/0.2.0/lagoon-cli-0.2.0-darwin-amd64.tar.gz"
+  sha256 "1a3ffe658ed1b710c150b21c2fd37f9be64f15bd043fc0e9e17a063ef13b68b3"
   version "0.2.0"
 
   bottle :unneeded
