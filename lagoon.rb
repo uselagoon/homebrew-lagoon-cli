adf852910f708857f0df424d86beedb1b2c8640c
Ben Jackson <b@benjackson.email>
2020-05-08 10:21:18 +1000

    wrong sha

--- lagoon.rb
+++ lagoon.rb
@@ -3,7 +3,7 @@ class Lagoon < Formula
   homepage "https://github.com/amazeeio/lagoon-cli"
   version  "0.9.0"
   url      "https://github.com/amazeeio/lagoon-cli/releases/download/0.9.0/lagoon-cli-0.9.0-darwin-amd64.tar.gz"
-  sha256   "408823ffa61bf08222a50ea0d4e771b06699e406fcfd98dcbd22f43a17f90716"
+  sha256   "7dca0aee2375f6e0c4a89ef2c866ac2c50f56a80eeb7c450395c0ce515b3e70b"
 
   bottle :unneeded
 
