183f73f89b88160ffbe5cd49040b31ef5a876be7
Ben Jackson <shreddedbacon@users.noreply.github.com>
2021-02-22 10:25:04 +1100

    Update lagoon.rb

--- lagoon.rb
+++ lagoon.rb
@@ -1,9 +1,9 @@
 class Lagoon < Formula
   desc     "Command line tool for interacting with a Lagoon API"
   homepage "https://github.com/amazeeio/lagoon-cli"
-  version  "0.11.0"
-  url      "https://github.com/amazeeio/lagoon-cli/releases/download/0.11.0/lagoon-cli-0.11.0-darwin-amd64.tar.gz"
-  sha256   "67bbe890a5bab2c780f9b16872d8f406a83cb33a90384d24b0cca440edf28489"
+  version  "0.11.1"
+  url      "https://github.com/amazeeio/lagoon-cli/releases/download/0.11.1/lagoon-cli-0.11.1-darwin-amd64.tar.gz"
+  sha256   "fc2f330667ce5f0abbea1c07970931d313bdb860abfbc7fcd72b603b1590a673"
 
   bottle :unneeded
 
